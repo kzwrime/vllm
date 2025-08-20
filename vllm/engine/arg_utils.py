c8e8971f300c5fc3a12c6c00b2552fadba174b7d
kunzh <RimeK20@outlook.com>
2025-08-20 02:05:41 +0000

    [Engine] Fix enable_chunked_prefill setting on CPU outside x86

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -1025,8 +1025,7 @@ class EngineArgs:
             self._set_default_args_v1(usage_context, model_config)
             # Disable chunked prefill for POWER (ppc64le)/ARM CPUs in V1
             if current_platform.is_cpu(
-            ) and current_platform.get_cpu_architecture() in (
-                    CpuArchEnum.POWERPC, CpuArchEnum.ARM):
+            ) and current_platform.get_cpu_architecture() != CpuArchEnum.X86:
                 logger.info(
                     "Chunked prefill is not supported for ARM and POWER CPUs; "
                     "disabling it for V1 backend.")
