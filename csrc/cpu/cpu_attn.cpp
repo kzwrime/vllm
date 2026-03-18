9f3b2aa185a048baddda9fefd0beaaa2d046e88a
caowanlu <caowanlu@xcoresigma.com>
2026-03-18 09:14:29 +0000

    [Fix] Fix attention compile fallback
    Signed-off-by: caowanlu <caowanlu@xcoresigma.com>

--- csrc/cpu/cpu_attn.cpp
+++ csrc/cpu/cpu_attn.cpp
@@ -80,20 +80,9 @@ torch::Tensor get_scheduler_metadata(
     const int64_t num_heads_kv, const int64_t head_dim,
     const torch::Tensor& seq_lens, at::ScalarType dtype,
     const torch::Tensor& query_start_loc, const bool casual,
-    const int64_t window_size, const std::string& isa_hint,
+    const int64_t window_size, const int64_t isa_hint,
     const bool enable_kv_split) {
-  cpu_attention::ISA isa;
-  if (isa_hint == "amx") {
-    isa = cpu_attention::ISA::AMX;
-  } else if (isa_hint == "vec") {
-    isa = cpu_attention::ISA::VEC;
-  } else if (isa_hint == "vec16") {
-    isa = cpu_attention::ISA::VEC16;
-  } else if (isa_hint == "neon") {
-    isa = cpu_attention::ISA::NEON;
-  } else {
-    TORCH_CHECK(false, "Unsupported CPU attention ISA hint: " + isa_hint);
-  }
+  cpu_attention::ISA isa = static_cast<cpu_attention::ISA>(isa_hint);
 
   cpu_attention::AttentionScheduler::ScheduleInput input;
   input.num_reqs = num_req;
@@ -144,14 +133,11 @@ void cpu_attn_reshape_and_cache(
     const torch::Tensor& key,    // [token_num, head_num, head_size]
     const torch::Tensor& value,  // [token_num, head_num, head_size]
     torch::Tensor&
-        key_cache,  // [num_blocks, num_kv_heads, block_size, head_size]
-    torch::Tensor&
-        value_cache,  // [num_blocks, num_kv_heads, block_size, head_size]
-    const torch::Tensor& slot_mapping, const std::string& isa) {
+        kv_cache,  // [2, num_blocks, num_kv_heads, block_size, head_size]
+    const torch::Tensor& slot_mapping, const int64_t isa) {
   TORCH_CHECK_EQ(key.dim(), 3);
   TORCH_CHECK_EQ(value.dim(), 3);
-  TORCH_CHECK_EQ(key_cache.dim(), 4);
-  TORCH_CHECK_EQ(value_cache.dim(), 4);
+  TORCH_CHECK_EQ(kv_cache.dim(), 5);
   TORCH_CHECK_EQ(key.stride(2), 1);
   TORCH_CHECK_EQ(value.stride(2), 1);
 
@@ -161,26 +147,16 @@ void cpu_attn_reshape_and_cache(
   const int64_t head_num = value.size(1);
   const int64_t key_head_num_stride = key.stride(1);
   const int64_t value_head_num_stride = value.stride(1);
-  const int64_t num_blocks = key_cache.size(0);
-  const int64_t num_blocks_stride = key_cache.stride(0);
-  const int64_t cache_head_num_stride = key_cache.stride(1);
-  const int64_t block_size = key_cache.size(2);
-  const int64_t block_size_stride = key_cache.stride(2);
+  const int64_t num_blocks = kv_cache.size(1);
+  const int64_t num_blocks_stride = kv_cache.stride(1);
+  const int64_t cache_head_num_stride = kv_cache.stride(2);
+  const int64_t block_size = kv_cache.size(3);
+  const int64_t block_size_stride = kv_cache.stride(3);
   const int64_t head_dim = key.size(-1);
+  const int64_t kv_stride = kv_cache.stride(0);
+  // auto kv_vec = kv_cache.unbind(0);
 
-  cpu_attention::ISA isa_tag = [&]() {
-    if (isa == "amx") {
-      return cpu_attention::ISA::AMX;
-    } else if (isa == "vec") {
-      return cpu_attention::ISA::VEC;
-    } else if (isa == "vec16") {
-      return cpu_attention::ISA::VEC16;
-    } else if (isa == "neon") {
-      return cpu_attention::ISA::NEON;
-    } else {
-      TORCH_CHECK(false, "Invalid ISA type: " + isa);
-    }
-  }();
+  cpu_attention::ISA isa_tag = static_cast<cpu_attention::ISA>(isa);
 
   VLLM_DISPATCH_FLOATING_TYPES(
       key.scalar_type(), "cpu_attn_reshape_and_cache", [&]() {
@@ -188,8 +164,10 @@ void cpu_attn_reshape_and_cache(
           CPU_ATTN_DISPATCH_IMPL(isa_tag, [&]() {
             attn_impl::reshape_and_cache(
                 key.data_ptr<scalar_t>(), value.data_ptr<scalar_t>(),
-                key_cache.data_ptr<scalar_t>(),
-                value_cache.data_ptr<scalar_t>(),
+                kv_cache.data_ptr<scalar_t>(),
+                kv_cache.data_ptr<scalar_t>() + kv_stride,
+                // kv_vec[0].data_ptr<scalar_t>(),
+                // kv_vec[1].data_ptr<scalar_t>(),
                 slot_mapping.data_ptr<int64_t>(), token_num,
                 key_token_num_stride, value_token_num_stride, head_num,
                 key_head_num_stride, value_head_num_stride, num_blocks,
@@ -203,10 +181,8 @@ void cpu_attn_reshape_and_cache(
 void cpu_attention_with_kv_cache(
     const torch::Tensor& query,  // [num_tokens, num_heads, head_size]
     const torch::Tensor&
-        key_cache,  // [num_blocks, num_kv_heads, block_size, head_size]
-    const torch::Tensor&
-        value_cache,        // [num_blocks, num_kv_heads, block_size, head_size]
-    torch::Tensor& output,  // [num_tokens, num_heads, head_size]
+        kv_cache,  // [2, num_blocks, num_kv_heads, block_size, head_size]
+    torch::Tensor& output,                 // [num_tokens, num_heads, head_size]
     const torch::Tensor& query_start_loc,  // [num_tokens + 1]
     const torch::Tensor& seq_lens,         // [num_tokens]
     const double scale, const bool causal,
@@ -218,24 +194,25 @@ void cpu_attention_with_kv_cache(
 ) {
   TORCH_CHECK_EQ(query.dim(), 3);
   TORCH_CHECK_EQ(query.stride(2), 1);
-  TORCH_CHECK_EQ(key_cache.dim(), 4);
-  TORCH_CHECK_EQ(value_cache.dim(), 4);
+  TORCH_CHECK_EQ(kv_cache.dim(), 5);
 
   cpu_attention::AttentionInput input;
   input.metadata = reinterpret_cast<cpu_attention::AttentionMetadata*>(
       scheduler_metadata.data_ptr());
   input.num_tokens = query.size(0);
   input.num_heads = query.size(1);
-  input.num_kv_heads = key_cache.size(1);
-  input.block_size = key_cache.size(2);
+  input.num_kv_heads = kv_cache.size(2);
+  input.block_size = kv_cache.size(3);
   input.query = query.data_ptr();
   input.query_num_tokens_stride = query.stride(0);
   input.query_num_heads_stride = query.stride(1);
-  input.cache_num_blocks_stride = key_cache.stride(0);
-  input.cache_num_kv_heads_stride = key_cache.stride(1);
+  input.cache_num_blocks_stride = kv_cache.stride(1);
+  input.cache_num_kv_heads_stride = kv_cache.stride(2);
+  const int64_t kv_stride = kv_cache.stride(0);
   input.blt_num_tokens_stride = block_table.stride(0);
-  input.key_cache = key_cache.data_ptr();
-  input.value_cache = value_cache.data_ptr();
+  // auto kv_vec = kv_cache.unbind(0);
+  // input.key_cache = kv_vec[0].data_ptr();
+  // input.value_cache = kv_vec[1].data_ptr();
   input.output = output.data_ptr();
   input.query_start_loc = query_start_loc.data_ptr<int32_t>();
   input.seq_lens = seq_lens.data_ptr<int32_t>();
@@ -259,6 +236,8 @@ void cpu_attention_with_kv_cache(
       query.scalar_type(), "cpu_attention_with_kv_cache", [&]() {
         CPU_ATTN_DISPATCH_CASE_HEADDIM(query.size(2), [&] {
           CPU_ATTN_DISPATCH_IMPL(input.metadata->isa, [&]() {
+            input.key_cache = kv_cache.data_ptr<scalar_t>();
+            input.value_cache = kv_cache.data_ptr<scalar_t>() + kv_stride;
             TORCH_CHECK_EQ(input.block_size % attn_impl::BlockSizeAlignment, 0);
             cpu_attention::AttentionMainLoop<attn_impl> mainloop;
             mainloop(&input);
