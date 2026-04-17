829b74613733cfb3668a8b9975b7345cf31bf257
kunzh <zhikun.wu@outlook.com>
2026-04-17 11:20:04 +0800

    [Profiler] Output profiler results to csv
    Signed-off-by: kunzh <zhikun.wu@outlook.com>

--- vllm/profiler/wrapper.py
+++ vllm/profiler/wrapper.py
@@ -1,9 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
+import csv
 from abc import ABC, abstractmethod
 from collections.abc import Callable
 from contextlib import nullcontext
+from datetime import datetime
 from typing import Literal
 
 import torch
@@ -266,6 +268,27 @@ class TorchProfilerWrapper(WorkerProfiler):
             # only print profiler results on rank 0
             if rank == 0:
                 print(table)
+
+            ts = datetime.timestamp(datetime.now())
+
+            profiler_dir = profiler_config.torch_profiler_dir
+            profiler_out_file = f"{profiler_dir}/profiler_out_{rank}_{ts}.txt"
+            sort_key = "self_cpu_time_total"
+            table = self.profiler.key_averages().table(sort_by=sort_key)
+
+            with open(profiler_out_file, "w") as f:
+                print(table, file=f)
+
+            key_averages_events = self.profiler.key_averages()
+            profiler_csv_file = f"{profiler_dir}/profiler_out_{rank}_{ts}.csv"
+            headers = list(vars(key_averages_events[0]).keys())
+
+            with open(profiler_csv_file, "w", newline="") as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(headers)
+                for event in key_averages_events:
+                    writer.writerow(vars(event).values())
+
         if self.dump_cpu_time_total and rank == 0:
             logger.info(
                 self.profiler.key_averages().table(
