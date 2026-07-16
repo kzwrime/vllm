25965022ac8a3e47c71a00235ffae6d34be17c45
Claire <31379115+Claireeeee@users.noreply.github.com>
2026-07-16 23:08:29 +0800

    [Fix][GDN] Fix startup errors in GDN models & hybrid attention for vllm v0.24.0(#7)
    Signed-off-by: caowanlu <caowanlu@xcoresigma.com>
    Co-authored-by: caowanlu <caowanlu@xcoresigma.com>
    
    Signed-off-by: kunzh <zhikun.wu@outlook.com>

--- vllm/v1/core/kv_cache_utils.py
+++ vllm/v1/core/kv_cache_utils.py
@@ -1072,6 +1072,10 @@ def unify_kv_cache_spec_page_size(
         return kv_cache_spec
 
     max_page_size = max(page_sizes)
+    # Note: Add new strategy for page_size unification.
+    for page_size in page_sizes:
+        if max_page_size % page_size != 0:
+            max_page_size = (max_page_size + page_size - 1) // page_size * page_size
     new_kv_cache_spec = {}
     for layer_name, layer_spec in kv_cache_spec.items():
         if layer_spec.page_size_bytes == max_page_size:
@@ -1082,7 +1086,7 @@ def unify_kv_cache_spec_page_size(
                 ratio = max_page_size // layer_page_size
                 new_block_size = layer_spec.block_size * ratio
                 new_spec = replace(layer_spec, block_size=new_block_size)
-            elif (
+            elif isinstance(layer_spec, MambaSpec) or (
                 isinstance(layer_spec, AttentionSpec)
                 and layer_spec.indexes_kv_by_block_stride
             ):
